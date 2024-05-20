78026ef08146e17fcb4e3368d99a872b254de904
Rodrigo Sakae <devsakae@gmail.com>
2024-05-19 22:22:31 -0300

    chore: css changes

--- src/app/components/Tigrebot/LastMessages.tsx
+++ src/app/components/Tigrebot/LastMessages.tsx
@@ -23,42 +23,35 @@ const LastMessages = () => {
       .then((res) => res.json())
       .then((data) => setMessages(data))
       .catch((error) => alert(error.message))
-      .finally(() => {
-        setLoading(false)
-  });
+      .finally(() => setLoading(false));
   }
 
   useEffect(() => {
     getMessages();
   }, [])
 
-  // useEffect(() => {
-
-  // }, [messages])
-
   return (
-    <div className='flex flex-col items-center w-full'>
-      <main className='p-2 rounded-2xl mb-4'>
+    <div className='flex flex-col'>
+      <main className='flex items-center w-full justify-center'>
         { loading
           ? (<Loading />)
-          : (<>
+          : (<div className='flex flex-col items-center'>
               { messages?.map((m: mongoMessages) => (<SingleMessage msg={ m } key={ m._id } />)) }
-              <article className='ml-8 mb-2 flex gap-2 items-start justify-start'>
-              <div className='min-w-10'>
-                <Image src={"/tigrebot.png"} alt='Tigrebot' width={70} height={70} className='rounded-full' />
-              </div>
-              <div className="max-w-5xl rounded-lg rounded-tl-none my-1 mb-4 px-3 py-2 text-sm bg-white flex flex-col">
-                  <h2 className='text-blue-500 font-semibold mb-2'>Tigrebot</h2>
-                  <p className='mb-2'>Gostou? No nosso grupo TigreLOG no WhatsApp, tem quiz, bolão, piadinha...</p>
-                  <p>Acesse já por <a href={ inviteLink }>clicando aqui nesse convite especial</a>! 🐯</p>
-                  <p className="text-gray-600 text-xs mt-1 text-right leading-none">AGORA É A HORA</p>
-              </div>
-            </article>
-             </>) }
+              <article className='flex items-start w-full gap-1 pr-1 md:w-2/3 md:justify-center'>
+                <div className='min-w-[45px] px-1'>
+                  <Image src={"/tigrebot.png"} alt='Tigrebot' width={42} height={42} className='rounded-full' />
+                </div>
+                <div className="rounded-lg rounded-tl-none mb-4 px-2 text-sm w-full bg-white flex flex-col">
+                    <h2 className='text-blue-500 font-semibold mt-1 mb-2'>Tigrebot</h2>
+                    <span>Gostou? No nosso grupo TigreLOG no WhatsApp, tem quiz, bolão, piadinha...<br /><br />Acesse já <a href={ inviteLink }>clicando aqui nesse convite especial</a>! 🐯</span>
+                    <p className="text-gray-600 text-xs mt-1 mb-2 text-right leading-none">AGORA Ẽ A HORA</p>
+                </div>
+              </article>
+             </div>) }
       </main>
-      <footer className='flex items-center justify-center w-full bg-slate-400 bottom-0 fixed h-8 text-sm'>
+      {/* <footer className='flex items-center justify-center w-full bg-slate-400 bottom-0 fixed h-8 text-sm'>
         <p>Criado e desenvolvido por Rodrigo Sakae em 2023</p>
-      </footer>
+      </footer> */}
     </div>
   )
 }
--- src/app/components/Tigrebot/SingleMessage.tsx
+++ src/app/components/Tigrebot/SingleMessage.tsx
@@ -4,14 +4,14 @@ import React from 'react'
 const SingleMessage = ({ msg }: any) => {
   const data = new Date(msg.data);
   return (
-    <article className='ml-8 mb-2 flex gap-2 items-start justify-start'>
-      <div className='min-w-10'>
-        <Image src={"/tigrebot.png"} alt='Tigrebot' width={70} height={70} className='rounded-full' />
+    <article className='flex items-start w-full gap-1 pr-1 md:w-2/3 md:justify-center'>
+      <div className='min-w-[45px] px-1'>
+        <Image src={"/tigrebot.png"} alt='Tigrebot' width={42} height={42} className='rounded-full' />
       </div>
-      <div className="max-w-5xl rounded-lg rounded-tl-none my-1 mb-4 px-3 py-2 text-sm bg-white flex flex-col">
-          <h2 className='text-blue-500 font-semibold mb-2'>Tigrebot</h2>
+      <div className="rounded-lg rounded-tl-none mb-4 px-2 text-sm w-full bg-white flex flex-col">
+          <h2 className='text-blue-500 font-semibold mt-1 mb-2'>Tigrebot</h2>
           { msg.mensagem.split(/\n/).map((l: string) => (<>{l}<br /></>)) }
-          <p className="text-gray-600 text-xs mt-1 text-right leading-none">{ data.toLocaleString('pt-br') }</p>
+          <p className="text-gray-600 text-xs mt-1 mb-2 text-right leading-none">{ data.toLocaleString('pt-br') }</p>
       </div>
     </article>
   )
