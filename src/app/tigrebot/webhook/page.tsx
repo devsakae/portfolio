225a2170ad476be4d0ca511d6bf77998a0a29f4c
Rodrigo Sakae <devsakae@gmail.com>
2024-09-01 07:14:43 -0300

    test: webhook

---
+++ src/app/tigrebot/webhook/page.tsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react"
+
+const page = () => {
+  return (
+    <Fragment>
+      <main className="flex place-content-center place-items-center h-screen">
+        <p>Hello.</p>
+      </main>
+    </Fragment>
+  )
+}
+
+export default page
