c664b03c5ff301a9aa5fce0e48ac34ff1eb98fbc
Rodrigo Sakae <devsakae@gmail.com>
2024-09-17 09:34:26 -0300

    devsakae.vercel.app/politica

---
+++ src/app/politica/page.tsx
@@ -0,0 +1,23 @@
+
+const page = () => {
+  return (
+    <main className="flex place-content-center place-items-center h-screen">
+    <div>
+        <h2>Política de Privacidade</h2>
+        <p>Todas as mensagens aqui publicadas são públicas, e podem ser acessadas por meio de convite do usuário pelo administrador do projeto Tigrebot, que pode ser contatado por meio das redes sociais disponíveis em <a href="https://devsakae.vercel.app/tigrebot">devsakae.vercel.app/tigrebot</a>.</p>
+        <p>Não registramos nenhum cookie em seu navegador sem o seu consentimento.</p>
+        <p>Qualquer forma de registro de seu acesso segue estritamente o disposto nas permissões legais dispostas na LGPD, a Lei Geral de Proteção de Dados - Lei 13.709/18.</p>
+        <p>Em caso de dúvidas, entre conosco diretamente pelo e-mail devsakae@gmail.com.</p>
+    </div>
+    <div>
+        <h2>Privacy Policy</h2>
+        <p>All messages published here are public, and can be easily accessed when joining the group mantained by Tigrelog project administrator, which can be contacted by any social media contact published at <a href="https://devsakae.vercel.app/tigrebot">devsakae.vercel.app/tigrebot</a>.</p>
+        <p>There is no cookie or registry saved in your browser without your agreement.</p>
+        <p>Any eventual type of access registry is anonimous, strictly following the regulatory laws such as LGPD - Persona Data Privacy Law of Brasil (Law n. 13.709/18)</p>
+        <p>Remaining questions about the case of use can be required by e-mail devsakae@gmail.com.</p>
+    </div>
+    </main>
+  )
+}
+
+export default page
