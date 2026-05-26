190a36c9aef0f34400022e771f3c7b22bda36346
Rodrigo Sakae <devsakae@gmail.com>
2026-05-25 22:01:51 -0300

    feat: new futebolsc -> futebolsc.vercel.app

--- src/app/futebolsc/page.tsx
+++ src/app/futebolsc/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation';
 
 export default async function page() {
-    redirect('https://futebolsc--00b8m9334y.expo.app/');
+    redirect('https://futebolscjogos.vercel.app');
 }
 
